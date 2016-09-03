8bb4414277cbf1b0642c783fee36d81b405503cd
princedc <prince.devasitham@gmail.com>
2016-09-03 09:50:17 +0530

    second commit

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ var server = http.createServer(function (request, response) {
   response.end("Hello World\n");
 });
 
+
+var port = process.env.PORT || 8080;
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+server.listen(port);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
