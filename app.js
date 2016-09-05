a9bf759c05c0123dc75513d8f1db5087e4016e24
princedc <prince.devasitham@gmail.com>
2016-09-05 08:30:08 +0530

    'commit'

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var http = require('http');
 
+
 //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
 var options = {
   host: 'tmweb.pacebus.com',
         path: '/TMWebWatch/LiveADAArrivalTimes?r=44&d=1&s=7871'
 };
 
+  var str = '';
+http.createServer(function (req, res) {
+  
+  
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    //'Content-Length': html.length,
+    'Expires': new Date().toUTCString()
+  });
 callback = function(response) {
   var str = '';
 
@@ -18,8 +28,21 @@ callback = function(response) {
   response.on('end', function () {
     var matchExpression = new RegExp(/<a class="adatime" title="\d+:\d+">(\d+:\d+)/m);
     var matchComponents = matchExpression.exec(str);
-    response.write(matchComponents[1]);
+    //response.write(matchComponents[1]);
+	res.end(matchComponents[1]);
   });
+
+
+
 }
 
 http.request(options, callback).end();
+
+
+  
+}).listen(process.env.PORT || 8080);
+
+
+
+
+
